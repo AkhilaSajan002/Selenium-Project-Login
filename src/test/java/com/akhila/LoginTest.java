84d0f3705ba58f8d5d01a049c4c835c08c93c2ba
Akhila S Nair <akhilasajan002@gmail.com>
2026-06-21 22:36:53 +0530

    Added POM Implementation Project

--- src/test/java/com/akhila/LoginTest.java
+++ src/test/java/com/akhila/LoginTest.java
@@ -1,7 +1,9 @@
 package com.akhila;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.akhila.pages.LoginPage;
 
 public class LoginTest {
 
@@ -11,47 +13,20 @@ public class LoginTest {
 
         driver.get("https://the-internet.herokuapp.com/login");
 
-        driver.findElement(By.id("username"))
-              .sendKeys(username);
+        LoginPage loginPage = new LoginPage(driver);
 
-        driver.findElement(By.id("password"))
-              .sendKeys(password);
-
-        driver.findElement(By.cssSelector("button[type='submit']"))
-              .click();
-
-        String message = driver.findElement(By.id("flash"))
-                               .getText();
+        loginPage.enterUsername(username);
+        loginPage.enterPassword(password);
+        loginPage.clickLogin();
 
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
-        System.out.println("Result: " + message);
-        System.out.println("-------------------------");
 
-        driver.quit();
+        //driver.quit();
     }
 
     public static void main(String[] args) {
 
-        // Valid username + Valid password
         login("tomsmith", "SuperSecretPassword!");
-
-        // Valid username + Invalid password
-        login("tomsmith", "wrongpass");
-
-        // Invalid username + Valid password
-        login("wronguser", "SuperSecretPassword!");
-
-        // Invalid username + Invalid password
-        login("wronguser", "wrongpass");
-
-        // Blank username
-        login("", "SuperSecretPassword!");
-
-        // Blank password
-        login("tomsmith", "");
-
-        // Blank username and password
-        login("", "");
     }
 }
